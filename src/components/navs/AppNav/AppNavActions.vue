b5663bff5a6b34b28dcbcce82e337b2256d6c14b
reshreshus <reshreshus@gmail.com>
2022-03-04 14:24:32 +0300

    fix AppNavActions connect btn

--- src/components/navs/AppNav/AppNavActions.vue
+++ src/components/navs/AppNav/AppNavActions.vue
@@ -16,6 +16,7 @@
       <span class="lg:hidden" v-text="$t('connect')" />
     </BalBtn> -->
     <button
+      v-else
       class="py-2 px-8 text-2xl text-green border-green font-secondary rounded-3xl border-2"
       @click="toggleWalletSelectModal"
     >
